b386f38e4df8f04f13fb923cf555c184c79e527c
ivecera <ivan@cera.cz>
2024-07-19 19:59:50 +0800

    psyche: sensors: Build UDFPS sensors sub-HAL
    * Build and ship UDFPS sensor sub-HAL for devices with UDFPS
    * Allow sensors HAL access /sys/touchpanel directory
    
    Change-Id: I440350e0d4d4296cbc77e68e8f87cc727b08528b

--- device.mk
+++ device.mk
@@ -233,7 +233,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
 PRODUCT_PACKAGES += \
-    libudfpshandler
+    libudfpshandler \
+    sensors.udfps
 
 PRODUCT_PACKAGES += \
     vendor.goodix.hardware.biometrics.fingerprint@2.1.vendor
